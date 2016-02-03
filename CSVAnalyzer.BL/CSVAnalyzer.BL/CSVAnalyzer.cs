d77765bae7fa906cfd9f946f5cfa080765d4410b
Saphora <w.vandermeer@workingspirit.nl>
2016-02-03 19:20:00 +0100

    first

---
+++ CSVAnalyzer.BL/CSVAnalyzer.BL/CSVAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FeedAnalyzer.BL
+{
+    public class CSVProvider
+    {
+        private string _CSVString = "";
+        private List<IDataRecord> Records = new List<IDataRecord>();
+
+        //open csv file for reading
+        public void Open(string file)
+        {
+            if (File.Exists(file))
+            {
+
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+
+    }
+}
